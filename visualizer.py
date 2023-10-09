4dafa00fa0840d15c788b4309eef0d943b59c1c6
xFooFoo <yarivfu@gmail.com>
2023-10-09 19:24:15 +1300

    Added .exe file to run the visualizer with NO SETUP!

--- visualizer.py
+++ visualizer.py
@@ -260,6 +260,8 @@ while running:
     event_list = pygame.event.get()
     for event in event_list:
         if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
             running = False
         #Event for loading iterator comes here, then we draw
     
